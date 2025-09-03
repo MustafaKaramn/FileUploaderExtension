f47b316d5f839f58d0e9d26460aff8ea5f193081
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-09-03 16:36:13 +0300

    manifest and config file updated to solve CORS policy error.

--- image-uploader-extension/vite.config.ts
+++ image-uploader-extension/vite.config.ts
@@ -7,4 +7,12 @@ export default defineConfig({
   plugins: [react(),
   crx({ manifest })
   ],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+    cors: true 
+  },
 })
