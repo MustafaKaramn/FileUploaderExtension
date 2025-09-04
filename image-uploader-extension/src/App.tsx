957ab2ad82fceb0e857a8dcdd618fb26fb085299
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-09-04 11:21:56 +0300

    Some necessary variables added to App component.

--- image-uploader-extension/src/App.tsx
+++ image-uploader-extension/src/App.tsx
@@ -1,7 +1,15 @@
 import { FiUploadCloud } from 'react-icons/fi'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [droppedFile, setDroppedFile] = useState<File | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const ALLOWED_TYPES = ['image/jpeg','image/jpg','image/png','image/webp','image/gif','image/jpeg',]
+
   return (
     <div className='container'>
       <div className='dropzone'>
