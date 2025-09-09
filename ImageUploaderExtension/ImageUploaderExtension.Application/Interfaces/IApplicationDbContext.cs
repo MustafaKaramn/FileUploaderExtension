25c9a4919f6087c2191cd6916e252a57e4310d49
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-09-09 10:06:52 +0300

    Interfaces created on Application layer.

---
+++ ImageUploaderExtension/ImageUploaderExtension.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ImageUploaderExtension.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageUploaderExtension.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<StoredFile> StoredFiles { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
