85fc5c82f18287cb59b711cb078ed2628a68acb5
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-09-09 12:23:28 +0300

    LocalStorageService implementations created.

--- ImageUploaderExtension/ImageUploaderExtension.Infrastructure/Services/LocalStorageService.cs
+++ ImageUploaderExtension/ImageUploaderExtension.Infrastructure/Services/LocalStorageService.cs
@@ -4,14 +4,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
 
 namespace ImageUploaderExtension.Infrastructure.Services
 {
     internal class LocalStorageService : IFileStorageService
     {
-        public Task<string> SaveFileAsync(Stream fileStream, string fileName, string mimeType)
+
+        private readonly IHostEnvironment _env;
+
+        public LocalStorageService(IHostEnvironment env)
         {
-            throw new NotImplementedException();
+            _env = env;
+        }
+
+        public async Task<string> SaveFileAsync(Stream fileStream, string fileName, string mimeType)
+        {
+            var uploadsFolderPath = Path.Combine(_env.ContentRootPath, "uploads");
+
+            if (!Directory.Exists(uploadsFolderPath))
+            {
+                Directory.CreateDirectory(uploadsFolderPath);
+            }
+
+            var fileExtension = Path.GetExtension(fileName);
+            var uniqueFileName = $"{Guid.NewGuid()}{fileExtension}";
+            var fullPath = Path.Combine(uploadsFolderPath, uniqueFileName);
+
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                await fileStream.CopyToAsync(stream);
+            }
+
+            return fullPath;
         }
     }
 }
