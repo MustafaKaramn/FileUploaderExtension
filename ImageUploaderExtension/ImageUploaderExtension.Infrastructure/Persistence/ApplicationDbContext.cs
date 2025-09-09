20450d4c73cf2d54f396936ef12d86845e835984
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-09-09 10:53:35 +0300

    Basic Infrastructure layer implementations.

---
+++ ImageUploaderExtension/ImageUploaderExtension.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ImageUploaderExtension.Application.Interfaces;
+using ImageUploaderExtension.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageUploaderExtension.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbSet<StoredFile> StoredFiles { get; set; }
+    }
+}
---
+++ ImageUploaderExtension/ImageUploaderExtension.Infrastructure/Services/LocalStorageService.cs
@@ -0,0 +1,17 @@
+﻿using ImageUploaderExtension.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageUploaderExtension.Infrastructure.Services
+{
+    internal class LocalStorageService : IFileStorageService
+    {
+        public Task<string> SaveFileAsync(Stream fileStream, string fileName, string mimeType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
