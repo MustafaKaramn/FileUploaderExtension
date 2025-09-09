988849b32a6989bf23468ebf0516ce491bfd832c
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-09-09 13:17:40 +0300

    Services, CORS, Rate Limiter added to Program.cs.

--- ImageUploaderExtension/ImageUploaderExtension.WebAPI/Program.cs
+++ ImageUploaderExtension/ImageUploaderExtension.WebAPI/Program.cs
@@ -1,23 +1,50 @@
+using ImageUploaderExtension.Infrastructure;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin() //Development ortamýnda çalýþmasý için
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("FileUploadPolicy", opt =>
+    {
+        opt.PermitLimit = 5;
+        opt.Window = TimeSpan.FromMinutes(1);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.QueueLimit = 5;
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+
 }
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
 app.UseAuthorization();
 
+app.UseCors();
+app.UseRateLimiter();
 app.MapControllers();
 
 app.Run();
