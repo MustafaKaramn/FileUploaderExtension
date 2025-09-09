3c108a0a1ea5b60f4b900c0251f4c764da5918c1
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-09-09 10:43:18 +0300

    UploadFileCommand command has been created according to CQRS design pattern.

---
+++ ImageUploaderExtension/ImageUploaderExtension.Application/Features/Files/Commands/UploadFileCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageUploaderExtension.Application.Features.Files.Commands
+{
+    public class UploadFileCommand : IRequest<string>
+    {
+        public Stream FileStream { get; set; }
+        public string FileName { get; set; }
+        public string MimeType { get; set; }
+        public int ExpirationDays { get; set; }
+    }
+}
