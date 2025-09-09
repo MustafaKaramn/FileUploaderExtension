ced20b71bef4d018312f59ac456e17c6d14e03a9
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-09-09 12:38:38 +0300

    DependencyInjection class created for set the Program.cs as minimal.

---
+++ ImageUploaderExtension/ImageUploaderExtension.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using ImageUploaderExtension.Application.Interfaces;
+using ImageUploaderExtension.Infrastructure.Persistence;
+using ImageUploaderExtension.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageUploaderExtension.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(x =>
+            {
+                x.UseSqlServer(connectionString);
+            });
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+            services.AddScoped<IFileStorageService, LocalStorageService>();
+
+            return services;
+        }
+    }
+}
