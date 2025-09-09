ede6e07d9d678f2c17eb9c92e7a44315313dd0ea
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-09-09 09:58:42 +0300

    StoredFile entity created.

---
+++ ImageUploaderExtension/ImageUploaderExtension.Domain/Entities/StoredFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageUploaderExtension.Domain.Entities
+{
+    public class StoredFile
+    {
+        public Guid Id { get; set; }
+        public string OriginalFileName { get; set; }
+        public string StoredFileName { get; set; }
+        public string Path { get; set; }
+        public string MimeType { get; set; }
+        public long SizeInBytes { get; set; }
+        public DateTime UploadedAt { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
