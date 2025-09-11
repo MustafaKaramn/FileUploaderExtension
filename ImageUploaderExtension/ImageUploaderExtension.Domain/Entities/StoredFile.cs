a60918b674c019f4c25ebe05b05ee031b985e000
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-09-11 10:51:15 +0300

    StoredFile entity updated with data annotations.

--- ImageUploaderExtension/ImageUploaderExtension.Domain/Entities/StoredFile.cs
+++ ImageUploaderExtension/ImageUploaderExtension.Domain/Entities/StoredFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,21 @@ namespace ImageUploaderExtension.Domain.Entities
     public class StoredFile
     {
         public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string OriginalFileName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string StoredFileName { get; set; }
+
+        [Required]
+        [MaxLength(1024)]
         public string Path { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string MimeType { get; set; }
         public long SizeInBytes { get; set; }
         public DateTime UploadedAt { get; set; }
